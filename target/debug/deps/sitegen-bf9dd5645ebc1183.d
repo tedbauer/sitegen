6f9a5a1e6290a12d5425cb3631d25fa1e534e89f
Theodore Bauer <ted@commure.com>
2021-12-17 20:43:20 -0500

    Initial upload

---
+++ target/debug/deps/sitegen-bf9dd5645ebc1183.d
@@ -0,0 +1,5 @@
+/Users/theodorebauer/Documents/nonwork/sitegen/target/debug/deps/sitegen-bf9dd5645ebc1183: src/main.rs
+
+/Users/theodorebauer/Documents/nonwork/sitegen/target/debug/deps/sitegen-bf9dd5645ebc1183.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/sitegen.d
@@ -0,0 +1,1 @@
+/Users/theodorebauer/Documents/nonwork/sitegen/target/debug/sitegen: /Users/theodorebauer/Documents/nonwork/sitegen/src/main.rs
